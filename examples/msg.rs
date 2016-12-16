d89484b6cd727b9baefbda71f064f98da6caa0f1
Michael Mokrysz <hi@46b.it>
2016-12-15 21:10:31 -0500

    Example of new protocol types working.

--- examples/msg.rs
+++ examples/msg.rs
@@ -10,4 +10,11 @@ fn main() {
     println!("{}", Yellow.bold().paint("Sirpent"));
 
     println!("{:?}", serde_json::to_string(&VersionMsg::new()));
+    println!("{:?}", serde_json::to_string(&PlainMessage::from_typed(VersionMsg::new())));
+
+    let s = serde_json::to_string(&PlainMessage::from_typed(VersionMsg::new())).unwrap();
+    let p: PlainMessage = serde_json::from_str(&*s).unwrap();
+    println!("{:?}", p);
+    let v: VersionMsg = p.to_typed().unwrap();
+    println!("{:?}", v);
 }
