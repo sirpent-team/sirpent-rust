09637f63073e402767c36e3bf0ac41a36ca72cc6
Michael Mokrysz <hi@46b.it>
2017-01-03 13:13:22 +0000

    Somewhat decoupled TCP server from main program.

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,11 @@ use std::str;
 use rand::OsRng;
 use std::collections::{HashSet, HashMap};
 use std::time::Duration;
+use std::thread;
 
 use futures::{Future, BoxFuture, Stream};
 use tokio_core::net::TcpListener;
-use tokio_core::reactor::Core;
+use tokio_core::reactor::{Core, Remote};
 use tokio_core::io::Io;
 
 use sirpent::*;
@@ -47,6 +48,50 @@ fn main() {
     let names: Arc<Mutex<HashSet<String>>> = Arc::new(Mutex::new(HashSet::new()));
     let players = Arc::new(Mutex::new(vec![]));
 
+    // Run TCP server to welcome clients and register them as players.
+    let handle = lp.handle();
+    handle.spawn(server(listener,
+                        lp.remote(),
+                        names.clone(),
+                        grid.clone(),
+                        timeout.clone(),
+                        players.clone()));
+
+    // @TODO: As the server is only being spawned there is no *need* for a second thread.
+    // The integration of games with the rest of the program is unclear but bear the above in mind.
+    //
+    // Game requirements:
+    // * Take existing player clients and play a game of sirpent with them until completion.
+    // * Once game is concluded return player clients to the pool.
+    // * After a short wait duration play a new game, as before with all pooled player clients.
+    // * Continue indefinitely.
+    //let names_ref = names.clone();
+    let players_ref = players.clone();
+    //let timeout_ref = timeout.clone();
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(10));
+
+        let engine = Engine::new(OsRng::new().unwrap(), grid.clone());
+        let mut players_lock = players_ref.lock().unwrap();
+        let games = play_game(engine, players_lock.drain(..).collect(), timeout);
+
+        let mut lp = Core::new().unwrap();
+        lp.run(games).unwrap();
+    });
+
+    // Poll event loop to keep program running.
+    loop {
+        lp.turn(None);
+    }
+}
+
+fn server(listener: TcpListener,
+          remote_handle: Remote,
+          names: Arc<Mutex<HashSet<String>>>,
+          grid: Grid,
+          timeout: Option<Duration>,
+          players: Arc<Mutex<Vec<Client>>>)
+          -> BoxFuture<(), ()> {
     let clients = listener.incoming()
         .map_err(|e| ProtocolError::from(e))
         .map(move |(socket, addr)| {
@@ -55,66 +100,54 @@ fn main() {
             (tell_handshake(transport, VersionMsg::new()), addr)
         });
 
-    let server = clients.for_each(|(client, addr)| {
-        let names_ref = names.clone();
-        let players_ref = players.clone();
-        let client_future =
-            client.and_then(move |(identify_msg, transport)| {
-                    // Find an unused name based upon the desired_name.
-                    // Subtly coded to ensure `names` is locked to ensure unique name still free.
-                    let mut name = identify_msg.desired_name;
-                    loop {
-                        let mut names_ref = names_ref.lock().unwrap();
-                        if names_ref.contains(&name) {
-                            name += "_";
-                        } else {
-                            // Reserve the new name.
-                            names_ref.insert(name.clone());
-                            break;
-                        }
-                    }
+    let server = clients.for_each(move |(transport, _)| {
+            // @TODO: If and when I build a client object, keep addr handy in it.
+            let mut names_ref = names.clone();
+            let players_ref = players.clone();
 
-                    let welcome_msg = WelcomeMsg {
-                        name: name.clone(),
-                        grid: grid.clone(),
-                        timeout: timeout,
-                    };
-                    tell_welcome(transport, welcome_msg).map(move |transport| (name, transport))
+            // Find a unique name for the Client and then send WelcomeMsg.
+            let client_future = transport.and_then(move |(identify_msg, transport)| {
+                let name = find_unique_name(&mut names_ref, identify_msg.desired_name);
+                let welcome_msg = WelcomeMsg {
+                    name: name.clone(),
+                    grid: grid,
+                    timeout: timeout,
+                };
+                tell_welcome(transport, welcome_msg).map(|transport| (name, transport))
+            });
+            // Queue the Client as a new player.
+            let client_future = client_future.map(move |(name, transport)| {
+                    players_ref.lock().unwrap().push((name, transport));
+                    ()
                 })
-                .then(move |result| {
-                    match result {
-                        Ok((name, transport)) => {
-                            players_ref.lock().unwrap().push((name, transport));
-                        }
-                        Err(e) => println!("Error welcoming client: {:?}", e),
-                    };
-                    //                Ok(futures::done(Ok(5)))
-                    //            })
-                    //            .and_then(|_| {
-                    // if players_ref.lock().unwrap().len() > 3 {
-                    let mut players_lock = players_ref.lock().unwrap();
-                    let game_players_future =
-                        futurise_and_join(players_lock.drain(..), |(msg, transport)| {
-                            futures::done(Ok((msg, transport))).boxed()
-                        });
+                .map_err(|e| {
+                    println!("Error welcoming client: {:?}", e);
+                    ()
+                });
 
-                    let grid_ref = grid.clone();
-                    let timeout_ref = timeout.clone();
-                    let play_game_future = game_players_future.and_then(move |game_player2s| {
-                        let engine = Engine::new(OsRng::new().unwrap(), grid_ref.clone());
-                        play_game(engine, game_player2s, timeout_ref)
-                    });
-                    return play_game_future.map_err(|_| ()).map(|_| ());
-                    // }
-                    // Ok(())
-                })
-                .boxed();
+            remote_handle.spawn(|_| client_future.boxed());
+            Ok(())
+        })
+        .then(|_| Ok(()));
 
-        handle.spawn(client_future);
-        Ok(())
-    });
+    return server.boxed();
+}
 
-    lp.run(server).unwrap();
+/// Find an unused name based upon the desired_name.
+fn find_unique_name(names: &mut Arc<Mutex<HashSet<String>>>, desired_name: String) -> String {
+    let mut name = desired_name;
+    loop {
+        // Locked once each iteration to ensure nothing can be added between the uniqueness
+        // check and the insertion.
+        let mut names_lock = names.lock().unwrap();
+        if names_lock.contains(&name) {
+            name += "_";
+        } else {
+            // Reserve the new name.
+            names_lock.insert(name.clone());
+            return name;
+        }
+    }
 }
 
 fn play_game(mut engine: Engine<OsRng>,
