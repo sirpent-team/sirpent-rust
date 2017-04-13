635f87fb6617ef44496161585d3125dcffdaf30e
Michael Mokrysz <hi@46b.it>
2017-04-13 01:38:20 +0100

    Adopted GameServerActor.
    This new main.rs code is highly compositional. This makes for a pleasant dataflow but I suspect leaves many edgecases unaddressed that will need examining.

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,9 @@ extern crate kabuki;
 
 use std::env;
 use std::str;
-use rand::OsRng;
-use std::thread;
+use rand::{Rng, OsRng};
 use std::net::SocketAddr;
-use futures::{future, stream, Future, Sink, Stream};
-use futures::future::Either;
+use futures::{stream, Future, Sink, Stream};
 use futures::sync::mpsc;
 use tokio_core::net::TcpListener;
 use tokio_core::reactor::Core;
@@ -71,25 +69,28 @@ fn main() {
                         queue_player_tx.clone(),
                         spectator_tx));
 
-    // @TODO: Game requirements:
-    // * Take existing player clients and play a game of sirpent with them until completion.
-    // * Once game is concluded return player clients to the pool.
-    // * After a short wait Milliseconds play a new game, as before with all pooled player clients.
-    // * Continue indefinitely.
-    thread::spawn(move || {
-        let mut lp = Core::new().unwrap();
-        let handle = lp.handle();
+    let game_actor = GameActor::new(timer.clone(), spectator_msg_tx);
+    let game_actor_ref = kabuki::Builder::new().spawn(&handle, game_actor);
 
-        let game_actor = GameActor::new(timer.clone(), spectator_msg_tx);
-        let game_actor = kabuki::Builder::new().spawn(&handle, game_actor);
+    let rng_fn = || -> Box<Rng> { return Box::new(OsRng::new().unwrap()) };
+    let game_server_actor = GameServerActor::new(rng_fn, grid, timeout, game_actor_ref);
+    let game_server_actor_ref = kabuki::Builder::new().spawn(&handle, game_server_actor);
 
-        lp.run(play_games(grid,
-                            queue_player_tx,
-                            Box::new(queue_player_rx.chunks(10)),
-                            game_actor,
-                            timeout))
-            .unwrap();
-    });
+    let gsw = queue_player_rx
+        .chunks(10)
+        .map(|players_vec| Room::new(players_vec.into_iter().collect()))
+        .and_then(move |players| game_server_actor_ref.clone().call(players))
+        .map(|(game, players)| {
+                 println!("End of game! {:?} {:?}",
+                          game.game_state(),
+                          game.round_state());
+                 let living_players = players.into_iter().filter(Client::is_connected);
+                 stream::iter(living_players.map(Ok))
+             })
+        .flatten()
+        .forward(queue_player_tx.sink_map_err(|_| ()))
+        .map(|_| ());
+    handle.spawn(gsw);
 
     // Poll event loop to keep program running.
     loop {
@@ -126,46 +127,4 @@ fn server(listener: TcpListener,
         })
         .then(|_| Ok(()));
     Box::new(server)
-}
-
-fn play_games(grid: Grid,
-              add_tx: mpsc::Sender<MsgClient<String>>,
-              add_rx: Box<Stream<Item = Vec<MsgClient<String>>, Error = ()>>,
-              mut game_actor: kabuki::ActorRef<(Game, MsgRoom<String>, Milliseconds),
-                                               (Game, MsgRoom<String>),
-                                               ()>,
-              timeout: Milliseconds)
-              -> Box<Future<Item = (), Error = ()>> {
-    let future = add_rx
-        .then(|res| match res {
-                  Ok(players_vec) => {
-                      if players_vec.is_empty() {
-                          Ok(None)
-                      } else {
-                          Ok(Some(players_vec))
-                      }
-                  }
-                  Err(()) => Ok(None),
-              })
-        .for_each(move |res| if let Some(players_vec) = res {
-                      let players = Room::new(players_vec.into_iter().collect());
-                      let game = Game::new(Box::new(OsRng::new().unwrap()), grid.clone());
-                      let add_tx = add_tx.clone();
-                      Either::A(game_actor
-                                    .call((game, players, timeout))
-                                    .and_then(move |(game, players)| {
-                println!("End of game! {:?} {:?}",
-                         game.game_state(),
-                         game.round_state());
-
-                let players_ok = players.into_iter().filter(Client::is_connected).map(Ok);
-                add_tx
-                    .send_all(stream::iter(players_ok))
-                    .map_err(|_| ())
-                    .map(|_| ())
-            }))
-                  } else {
-                      Either::B(future::ok(()))
-                  });
-    Box::new(future)
 }
